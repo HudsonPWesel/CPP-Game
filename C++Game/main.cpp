7c88458cc1d3a04863a7b80771ec485ebd847fba
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-09-14 07:53:37 -0400

    Initial Commit

---
+++ C++Game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++Game
+//
+//  Created by Hudson Wesel on 9/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
